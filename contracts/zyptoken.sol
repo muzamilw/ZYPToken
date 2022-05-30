b524fda83a804d198714f25f40531fe7c311fa32
Muzammil Waheed <muzamilw@hotmail.com>
2022-05-31 00:33:10 +0500

    speed bump implemented

--- contracts/zyptoken.sol
+++ contracts/zyptoken.sol
@@ -15,7 +15,7 @@ contract ZYPToken is ERC20, ERC20Burnable, Pausable, AccessControl {
     constructor() ERC20("ZYPToken", "ZYP") {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(PAUSER_ROLE, msg.sender);
-        _mint(msg.sender, 6000000000 * 10 ** decimals());
+        _mint(msg.sender, 200000000 * 10 ** decimals());
         _setupRole(MINTER_ROLE, msg.sender);
     }
 
@@ -31,11 +31,34 @@ contract ZYPToken is ERC20, ERC20Burnable, Pausable, AccessControl {
         _mint(to, amount);
     }
 
+    mapping (address => uint) private userLastAction;
+    uint throttleTime = 30 seconds; 
+
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
         whenNotPaused
         override
     {
         super._beforeTokenTransfer(from, to, amount);
+
+         if (userLastAction[msg.sender].length == 0) {
+            userLastAction[msg.sender] = 0;
+        }
+        require(block.timestamp - throttleTime >= userLastAction[msg.sender]);
+        userLastAction[msg.sender] = block.timestamp; // now == block.timestamp
+        
+    }
+
+    
+    //Attach this to critical functions, such as balance withdrawals
+    modifier speedBump() { 
+
+            
+       if (userLastAction[msg.sender].length == 0) {
+           userLastAction[msg.sender] = 0;
+       }
+       require(block.timestamp - throttleTime >= userLastAction[msg.sender]);
+       userLastAction[msg.sender] = block.timestamp; // now == block.timestamp
+       _;
     }
 }
